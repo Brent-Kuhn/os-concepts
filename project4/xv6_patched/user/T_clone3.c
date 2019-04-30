359e9406a129d51105a722fca5a2a58b5f7bb7a8
Greg Willard (r3pwn) <wickett06@gmail.com>
2019-04-30 19:58:22 -0400

    [tests] make T_clone3 not hang

--- project4/xv6_patched/user/T_clone3.c
+++ project4/xv6_patched/user/T_clone3.c
@@ -39,7 +39,7 @@ main(int argc, char *argv[])
    int clone_pid = clone(worker, 0, stack);
    printf(1, "I am thead pid %d\n", clone_pid);
    assert(clone_pid > 0);
-   while(!newfd);
+   assert(newfd);
    printf(1, "I am going insane?\n");
    assert(write(newfd, "goodbye\n", 8) == -1);
    printf(1, "TEST PASSED\n");
