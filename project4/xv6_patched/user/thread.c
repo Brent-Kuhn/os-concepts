b01dd7d577a92c82b5f04e35572726b61ba287b5
Greg Willard (r3pwn) <wickett06@gmail.com>
2019-04-30 19:44:29 -0400

    [user] add basic thread library support

--- project4/xv6_patched/user/thread.c
+++ project4/xv6_patched/user/thread.c
@@ -1,14 +1,33 @@
+#include "types.h"
+#include "user.h"
+#include "syscall.h"
 #include "thread.h"
+
+#define assert(x) if (x) {} else { \
+   printf(1, "%s: %d ", __FILE__, __LINE__); \
+   printf(1, "assert failed (%s)\n", # x); \
+   printf(1, "TEST FAILED\n"); \
+   exit(); \
+}
 
 int thread_create(void (*start_routine)(void*), void *arg)
 {
-    return 0;
+    int pgsz = 4096;
+    void *stack = malloc(pgsz*2);
+    assert(stack != NULL);
+    if((uint)stack % pgsz)
+        stack = stack + (pgsz - (uint)stack % pgsz);
+
+    int clone_pid = clone(start_routine, (void*)&arg, stack);
+    return clone_pid;
 }
 
 
 int thread_join()
 {
-    return 0;
+     void *join_stack;
+     int join_pid = join(&join_stack);
+     return join_pid;
 }
 
 
